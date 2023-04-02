f2ab0436baa54cb6a96b534fbc6b544bd480238e
tirth-bhagwat <tirth.bhagwat@proton.me>
2023-04-02 20:31:50 +0530

    add tests to stack

--- data-strustures/src/stack.rs
+++ data-strustures/src/stack.rs
@@ -24,11 +24,11 @@ impl Stack {
         }
     }
 
-    pub fn pop(&mut self) -> isize {
+    pub fn pop(&mut self) -> Result<isize, Box<dyn std::error::Error>> {
         if self.tos != -1 {
             let x = self.vector[self.tos as usize];
             self.tos -= 1;
-            return x;
+            return Ok(x);
         } else {
             panic!("Stack Underflow")
         }
@@ -43,5 +43,53 @@ impl Stack {
             print!("{} ", self.vector[i as usize]);
         }
         println!("");
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let stack = Stack::new(10);
+        assert_eq!(stack.tos, -1, "Invalid tos");
+        assert_eq!(stack.size, 10, "Invalid size of stack");
+        assert_eq!(stack.vector.len(), 0, "Vector is not empty");
+    }
+
+    #[test]
+    #[should_panic(expected = "Stack Overflow")]
+    fn test_push() {
+        let mut stack = Stack::new(5);
+
+        assert_eq!(stack.push(10), 10, "Invalid push");
+        assert_eq!(stack.push(20), 20, "Invalid push");
+        assert_eq!(stack.push(30), 30, "Invalid push");
+        assert_eq!(stack.push(40), 40, "Invalid push");
+        assert_eq!(stack.push(50), 50, "Invalid push");
+
+        assert_eq!(stack.push(50), 50, "Invalid push");
+    }
+
+    #[test]
+    fn test_pop() -> Result<(), Box<dyn std::error::Error>> {
+        let mut stack = Stack::new(5);
+
+        assert_eq!(stack.push(10), 10, "Cannot push");
+        assert_eq!(stack.push(20), 20, "Cannot push");
+        assert_eq!(stack.push(30), 30, "Cannot push");
+        assert_eq!(stack.push(40), 40, "Cannot push");
+        assert_eq!(stack.push(50), 50, "Cannot push");
+
+        assert_eq!(stack.pop()?, 50, "Invalid pop");
+        assert_eq!(stack.pop()?, 40, "Invalid pop");
+        assert_eq!(stack.pop()?, 30, "Invalid pop");
+        assert_eq!(stack.pop()?, 20, "Invalid pop");
+        assert_eq!(stack.pop()?, 10, "Invalid pop");
+
+        assert!(stack.pop().is_err(), "Stack is not empty");
+
+        Ok(())
     }
 }
